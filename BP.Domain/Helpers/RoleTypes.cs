3a195352844b621341a767e0e16ffa5eb3552cf5
Brandon Truong <brandon-truong@hotmail.com>
2012-03-14 19:54:42 +1100

    implemenyed user creation

---
+++ BP.Domain/Helpers/RoleTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace BP.Domain.Helpers
+{
+    public static class RoleTypes
+    {
+        public static string Admin = "admin";
+        public static string TeamLeader = "team leader";
+        public static string Approver = "approver";
+        public static string Author = "author";
+        public static string Buddy = "buddy";
+    }
+}
